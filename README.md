60e714bf510d91ddc31e3aa78dc458ef8036f0ed
Arjun Kakkar <arjunkakkar8@gmail.com>
2022-06-04 15:39:37 -0700

    Started broadband analysis with school districts

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# California Eduaction Analysis
+# California Education Analysis
+
 This is the project repository that contains all analysis scripts.
 
 For information about data sources and documentation, see the [Notion doc](https://www.notion.so/CA-Educational-Equity-Analysis-f5d92738a43c4dde8c885d514b8b6623)
+
+## There are two datasets required to replicate the analysis:
+
+Download these, include them in the data directory of the project and update the appropriate paths in the analysis scripts.
+
+-   [CA Broadband database](https://www.cpuc.ca.gov/-/media/cpuc-website/divisions/communications-division/zipped-files/broadband-mapping/cabroadbanddec2020publicgdb.zip)
+-   [CA School District Shapefile](https://gis.data.ca.gov/datasets/CDEGIS::california-school-district-areas-2020-21/about)
--- preliminary_analysis.R
+++ preliminary_analysis.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(data.table)
+library(sf)
+library(parallel)
 
 
 census <-
@@ -11,6 +13,45 @@ graduation <-
 career <-
   fread('https://www3.cde.ca.gov/researchfiles/cadashboard/ccidownload2021.txt')
 
+
+#########################################
+############# BROADBAND #################
+
+st_par <- function(sf_df, sf_func, n_cores, ...){
+  
+  # Create a vector to split the data set up by.
+  split_vector <- rep(1:n_cores, each = nrow(sf_df) / n_cores, length.out = nrow(sf_df))
+  
+  # Perform GIS analysis
+  split_results <- split(sf_df, split_vector) %>%
+    mclapply(function(x) sf_func(x, ...), mc.cores = n_cores)
+  
+  # Combine results back together. Method of combining depends on the output from the function.
+  if ( class(split_results[[1]]) == 'list' ){
+    result <- do.call("c", split_results)
+    names(result) <- NULL
+  } else {
+    result <- do.call("rbind", split_results)
+  }
+  
+  # Return result
+  return(result)
+}
+
+school_districts <-
+  st_read('California_School_District_Areas_2020-21')
+broadband <-
+  st_read('CA_Broadband_Dec2020_Public.gdb/', layer = "Fixed_Consumer_Deployment") %>%
+  st_transform(st_crs(school_districts))
+
+intersections <- st_intersects(school_districts, broadband)
+
+intersection_pct <- st_par(school_districts, st_intersection, 8, broadband)
+  
+  st_intersection(school_districts, broadband) %>%
+  mutate(pct = st_area(.))
+
+#st_layers('California_School_District_Areas_2020-21/')
 
 #############
 
@@ -184,7 +225,7 @@ absenteeism_16_20 %>%
   ) %>%
   ggplot() +
   geom_line(aes(
-    x = as.Date(AcademicYear, format="%Y"),
+    x = as.Date(AcademicYear, format = "%Y"),
     y = Rate,
     color = ReportingCategory,
     group = ReportingCategory
